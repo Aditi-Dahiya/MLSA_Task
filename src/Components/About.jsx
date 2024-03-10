c5b179b748e16f376b307d304ceae9e90f9f3e1a
Aditi <aditidahiya1976@gmail.com>
2024-03-10 18:41:31 +0530

    add changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "birds  background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a data science and machine learning enthusiast studying at DCRUST. I enjoy creating unique and simplistic solutions for users in creative ways.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
