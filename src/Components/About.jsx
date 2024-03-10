9227590fd0317ac63498928a324459ed9f9c3ca0
Aditi <aditidahiya1976@gmail.com>
2024-03-10 18:10:35 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -18,7 +18,7 @@ import React from "react";
  */
 import image from "../images/Birds.jpg";
 
-const imageAltText = "birds background";
+const imageAltText = "birds  background";
 
 /**
  * Sort description that expands on your title on the Home component.
